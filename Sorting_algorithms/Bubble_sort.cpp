5eb99ceb409e2a35bbf398b35ffa2239e6722d16
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-13 19:31:02 +0300

    Update Bubble_sort.cpp

--- Sorting_algorithms/Bubble_sort.cpp
+++ Sorting_algorithms/Bubble_sort.cpp
@@ -1,27 +1,20 @@
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-// Пузырьковая сортировка
-vector<int> bubble_sort(vector<int>& arr) {
-    // Получаем длину массива
+void bubble_sort(vector<int>& arr) {
     int n = arr.size();
-    // Проходим по всем элементам массива
-    for (int i = 0; i < n; ++i) {
-        // Установим флаг, который будет служить индикатором, были ли какие-либо обмены в этой итерации
-        bool swapped = false;
-        // Проходим по массиву сравнивая каждую пару соседних элементов
-        // и меняем их местами, если они находятся в неправильном порядке
-        for (int j = 0; j < n-i-1; ++j) {
-            if (arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        // Если в этой итерации не было ни одного обмена, то массив уже отсортирован
-        if (!swapped) {
+        if (!swapped)
             break;
-        }
     }
-    return arr;
 }
