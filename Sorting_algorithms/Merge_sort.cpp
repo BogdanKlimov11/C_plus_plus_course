b988c53020c834256f99a1552cbff040862053cc
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-13 20:20:13 +0300

    Update Merge_sort.cpp

--- Sorting_algorithms/Merge_sort.cpp
+++ Sorting_algorithms/Merge_sort.cpp
@@ -1,53 +1,51 @@
-#include <iostream>
 #include <vector>
 
 using namespace std;
 
-void merge(vector<int>& arr, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-    vector<int> left(n1), right(n2);
+    vector<int> L(n1);
+    vector<int> R(n2);
 
     for (int i = 0; i < n1; i++)
-        left[i] = arr[l + i];
+        L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
-        right[j] = arr[m + 1 + j];
+        R[j] = arr[mid + 1 + j];
 
     int i = 0;
     int j = 0;
-    int k = l;
-
+    int k = left;
     while (i < n1 && j < n2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
         } else {
-            arr[k] = right[j];
+            arr[k] = R[j];
             j++;
         }
         k++;
     }
 
     while (i < n1) {
-        arr[k] = left[i];
+        arr[k] = L[i];
         i++;
         k++;
     }
 
     while (j < n2) {
-        arr[k] = right[j];
+        arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void merge_sort(vector<int>& arr, int l, int r) {
-    if (l >= r) {
-        return;
+void merge_sort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
-    int m = l + (r - l) / 2;
-    merge_sort(arr, l, m);
-    merge_sort(arr, m + 1, r);
-    merge(arr, l, m, r);
 }
